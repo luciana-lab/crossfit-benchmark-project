c77d5fc99de27bdf6c5d3a1c2b354e337cbd6ae7
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-28 08:34:38 -0400

    scale and results for time methods

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,14 @@
 class Score < ApplicationRecord
     belongs_to :athlete
     belongs_to :workout
+
+    def scale
+        if self.rx == 0
+            rx = "scale"
+        end
+    end
+
+    def result_for_time
+        self.time.strftime("%M:%S")
+    end
 end
