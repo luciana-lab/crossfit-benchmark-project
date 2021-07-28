cdab5ea1afd53b0b31f49534b66ff09b2ac696bd
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-28 10:31:50 -0400

    workout routes, index view and controller

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,18 @@
+class WorkoutsController < ApplicationController
+    def index
+        @workouts = Workout.all
+    end
+
+    def show
+        @workout = Workout.find(id: params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+end
