843ee74bfd41cd576b132f48b090031e09246459
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-29 12:28:04 -0400

    created gyms controller, routes, views and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  
   resources :workouts do
     resources :scores, shallow: true
   end
+
+  resources :gyms
+
 end
