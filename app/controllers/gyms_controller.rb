f00b8d9b407cfc3d84507dcb22a17ebaaa854dde
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-29 12:52:25 -0400

    add gym new and create controller, view, and partials

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -6,4 +6,22 @@ class GymsController < ApplicationController
     def show
         @gym = Gym.find(params[:id])
     end
+
+    def new
+        @gym = Gym.new
+    end
+
+    def create
+        @gym = Gym.new(gym_params)
+        if @gym.save
+            redirect_to gym_path(@gym)
+        else
+            render :new
+        end
+    end
+
+    private
+    def gym_params
+        params.require(:gym).permit(:name, :website, :address)
+    end
 end
