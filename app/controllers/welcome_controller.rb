9f90f2be334af06b00656f427a7b3b688e12c6a7
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-02 17:21:13 -0400

    search by workout inside rooth page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
     def index
-        
+        @workouts = Workout.search(params[:search])
+    end
+
+    private
+    def workout_params
+        params.require(:workout).permit(:name, :category, :description, :id, :search)
     end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,19 +3,8 @@ class Workout < ApplicationRecord
     has_many :users, through: :scores
     validates :name, presence: true, uniqueness: true
     validates :category, :description, presence: true
-
-    # def workout=(hash_of_attributes)
-    #     if !hash_of_attributes["group"].blank?
-    #         self = self.find_or_create_by(hash_of_attributes)
-    #     end
-    # end
-
-    def group_other=(value)
-        self.group = value if group == 'Other'
-    end
-
-    def group_other
-        group
-    end
     
+    def self.search(search)
+        where("LOWER(name) LIKE :term OR LOWER(category) LIKE :term OR LOWER(description) LIKE :term", term: "%#{search}%")
+    end
 end
