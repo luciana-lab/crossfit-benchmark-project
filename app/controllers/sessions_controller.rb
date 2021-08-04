9bf2691de8eb0b8fdd3875e47bb67b33eadd7242
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-04 17:52:13 -0400

    login page style and welcome page button style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
+            flash[:message] = "Login failed: invalid email or password"
             redirect_to login_path
         end
     end
