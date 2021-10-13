1e32ce7bf7fcedc3be8a2d7973bd5dc59c9a314c
Luciana Sugeta <lulusugeta@gmail.com>
2021-10-13 14:11:19 -0400

    Update scope tfor postgresql

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,8 +4,8 @@ class Workout < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :category, :description, presence: true
 
-    scope :category_nav_selector, -> (category) {where('category == ?', category)}
-    scope :wod_group_selector, -> (wod_group) {where('wod_group == ?', wod_group)}
+    scope :category_nav_selector, -> (category) {where('category = ?', category)}
+    scope :wod_group_selector, -> (wod_group) {where('wod_group = ?', wod_group)}
     
     def self.search(search)
         where("LOWER(name) LIKE :term OR LOWER(wod_group) LIKE :term OR LOWER(category) LIKE :term OR LOWER(description) LIKE :term", term: "%#{search}%")
